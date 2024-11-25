14ed340e8e38c0def4e8b5d1212c6affe36ab172
SPennyh <87890006+SPennyh@users.noreply.github.com>
2024-11-25 00:25:28 -0800

    started layout

--- yelp_gui.py
+++ yelp_gui.py
@@ -2,6 +2,7 @@ import pyodbc
 from customtkinter import *
 import passwords
 
+
 def check_user_id(entry, parent, controller):
     user_id = entry.get()
     try:
@@ -19,13 +20,25 @@ def check_user_id(entry, parent, controller):
             raise ValueError("Invalid User ID")
     except (pyodbc.Error, ValueError) as ex:
         error_message = str(ex)
-        textbox = CTkTextbox(master=parent, width=100, corner_radius=0)
+        textbox = CTkTextbox(master=parent, width=200, height=50, corner_radius=5)
         textbox.place(relx=0.5, rely=0.3, anchor=CENTER)
         textbox.insert("0.0", f"Error: {error_message}")
     finally:
         cursor.close()
         connection.close()
 
+def load_buttons(self, controller):
+        button_border = CTkFrame(self, width=178, height=640, fg_color='#525252')
+        button_border.place(x=31, y=40)
+
+        logout_button = CTkButton(button_border, text="Logout", font=('arial bold',12), fg_color='#828AFF', corner_radius=15, text_color='black', command=lambda: controller.show_frame(loginPage))
+        logout_button.place(x=22, y=581)
+
+        business_button = CTkButton(button_border, text="Search Businesses", font=('arial bold',12), fg_color='#828AFF', corner_radius=15, text_color='black',command=lambda: controller.show_frame(searchBusiness))
+        business_button.place(x=22, y=22.42)
+
+        user_button = CTkButton(button_border, text="Search Users", font=('arial bold',12), fg_color='#828AFF', corner_radius=15, text_color='black',command=lambda: controller.show_frame(searchUsers))
+        user_button.place(x=22, y=76.43)
 
 class yelp_gui(CTk):
     def __init__(self, *args, **kwargs):
@@ -57,111 +70,77 @@ class loginPage(CTkFrame):
     def __init__(self, parent, controller):
         CTkFrame.__init__(self, parent)
 
-        user_box = CTkEntry(self, placeholder_text="uid")
-        user_box.place(relx=0.5, rely=0.45, anchor=CENTER)
+        title = CTkLabel(self, text="Yelp Database", font=('arial bold',40), text_color='#828AFF')
+        title.place(x=514, y=141)
+
+        entry_border = CTkFrame(self, width=300, height=143, fg_color='#525252')
+        entry_border.place(x=490, y=310)
+
+        user_title = CTkLabel(entry_border, text="User ID", font=('arial', 12), text_color='#828AFF')
+        user_title.place(x=22, y=19)
+
+        user_box = CTkEntry(entry_border, placeholder_text="Enter your User ID", width=256, height=30, fg_color='#D9D9D9', border_color='#828AFF', corner_radius=5, text_color='black')
+        user_box.place(x=22, y=41)
 
         button = CTkButton(
-            self,
-            text="Login",
-            command=lambda: check_user_id(user_box, self, controller)
+            entry_border,
+            text="Sign in",
+            font=('arial bold', 12),
+            command=lambda: check_user_id(user_box, self, controller), fg_color='#828AFF', corner_radius=5, width=256, height=31
         )
-        button.place(relx=0.5, rely=0.6, anchor=CENTER)
+        button.place(x=22, y=84)
 
 
 class mainPage(CTkFrame):
     def __init__(self, parent, controller):
         CTkFrame.__init__(self, parent)
 
-        logout_button = CTkButton(self, text="Logout", command=lambda: controller.show_frame(loginPage))
-        logout_button.place(relx=0.9, rely=0.9, anchor=CENTER)
+        entry_border = CTkFrame(self, width=994, height=118, fg_color='#525252')
+        entry_border.place(x=233, y=40)
 
-        business_button = CTkButton(self, text="Search Businesses", command=lambda: controller.show_frame(searchBusiness))
-        business_button.place(relx=0.1, rely=0.1, anchor=CENTER)
+        user_title = CTkLabel(entry_border, text="Yelp Database", font=('arial bold', 40), text_color='#828AFF')
+        user_title.place(x=16, y=39)
 
-        user_button = CTkButton(self, text="Search Users", command=lambda: controller.show_frame(searchUsers))
-        user_button.place(relx=0.3, rely=0.1, anchor=CENTER)
-
-        friend_button = CTkButton(self, text="Make a Friend", command=lambda: controller.show_frame(makeFriend))
-        friend_button.place(relx=0.5, rely=0.1, anchor=CENTER)
-
-        review_button = CTkButton(self, text="Review a Business", command=lambda: controller.show_frame(reviewBusiness))
-        review_button.place(relx=0.7, rely=0.1, anchor=CENTER)
+        load_buttons(self, controller)
 
 class searchBusiness(CTkFrame):
     def __init__(self, parent, controller):
         CTkFrame.__init__(self, parent)
 
-        logout_button = CTkButton(self, text="Logout", command=lambda: controller.show_frame(loginPage))
-        logout_button.place(relx=0.9, rely=0.9, anchor=CENTER)
+        entry_border = CTkFrame(self, width=994, height=118, fg_color='#525252')
+        entry_border.place(x=233, y=40)
 
-        business_button = CTkButton(self, text="Search Businesses", command=lambda: controller.show_frame(searchBusiness))
-        business_button.place(relx=0.1, rely=0.1, anchor=CENTER)
+        user_title = CTkLabel(entry_border, text="Yelp Database - Search for a Business", font=('arial bold', 40), text_color='#828AFF')
+        user_title.place(x=16, y=39)
 
-        user_button = CTkButton(self, text="Search Users", command=lambda: controller.show_frame(searchUsers))
-        user_button.place(relx=0.3, rely=0.1, anchor=CENTER)
+        load_buttons(self, controller)
 
-        friend_button = CTkButton(self, text="Make a Friend", command=lambda: controller.show_frame(makeFriend))
-        friend_button.place(relx=0.5, rely=0.1, anchor=CENTER)
-
-        review_button = CTkButton(self, text="Review a Business", command=lambda: controller.show_frame(reviewBusiness))
-        review_button.place(relx=0.7, rely=0.1, anchor=CENTER)
+        slider = CTkSlider(self, from_=0, to=5, number_of_steps=5)
+        slider.place(relx=0.3, rely=0.3, anchor=CENTER)
 
 class searchUsers(CTkFrame):
     def __init__(self, parent, controller):
         CTkFrame.__init__(self, parent)
 
-        logout_button = CTkButton(self, text="Logout", command=lambda: controller.show_frame(loginPage))
-        logout_button.place(relx=0.9, rely=0.9, anchor=CENTER)
+        entry_border = CTkFrame(self, width=994, height=118, fg_color='#525252')
+        entry_border.place(x=233, y=40)
 
-        business_button = CTkButton(self, text="Search Businesses", command=lambda: controller.show_frame(searchBusiness))
-        business_button.place(relx=0.1, rely=0.1, anchor=CENTER)
+        user_title = CTkLabel(entry_border, text="Yelp Database - Search for Users", font=('arial bold', 40), text_color='#828AFF')
+        user_title.place(x=16, y=39)
 
-        user_button = CTkButton(self, text="Search Users", command=lambda: controller.show_frame(searchUsers))
-        user_button.place(relx=0.3, rely=0.1, anchor=CENTER)
-
-        friend_button = CTkButton(self, text="Make a Friend", command=lambda: controller.show_frame(makeFriend))
-        friend_button.place(relx=0.5, rely=0.1, anchor=CENTER)
-
-        review_button = CTkButton(self, text="Review a Business", command=lambda: controller.show_frame(reviewBusiness))
-        review_button.place(relx=0.7, rely=0.1, anchor=CENTER)
+        load_buttons(self, controller)
 
 class makeFriend(CTkFrame):
     def __init__(self, parent, controller):
         CTkFrame.__init__(self, parent)
 
-        logout_button = CTkButton(self, text="Logout", command=lambda: controller.show_frame(loginPage))
-        logout_button.place(relx=0.9, rely=0.9, anchor=CENTER)
-
-        business_button = CTkButton(self, text="Search Businesses", command=lambda: controller.show_frame(searchBusiness))
-        business_button.place(relx=0.1, rely=0.1, anchor=CENTER)
-
-        user_button = CTkButton(self, text="Search Users", command=lambda: controller.show_frame(searchUsers))
-        user_button.place(relx=0.3, rely=0.1, anchor=CENTER)
-
-        friend_button = CTkButton(self, text="Make a Friend", command=lambda: controller.show_frame(makeFriend))
-        friend_button.place(relx=0.5, rely=0.1, anchor=CENTER)
-
-        review_button = CTkButton(self, text="Review a Business", command=lambda: controller.show_frame(reviewBusiness))
-        review_button.place(relx=0.7, rely=0.1, anchor=CENTER)
+        load_buttons(self, controller)
 
 class reviewBusiness(CTkFrame):
     def __init__(self, parent, controller):
         CTkFrame.__init__(self, parent)
 
-        logout_button = CTkButton(self, text="Logout", command=lambda: controller.show_frame(loginPage))
-        logout_button.place(relx=0.9, rely=0.9, anchor=CENTER)
-
-        business_button = CTkButton(self, text="Search Businesses", command=lambda: controller.show_frame(searchBusiness))
-        business_button.place(relx=0.1, rely=0.1, anchor=CENTER)
-
-        user_button = CTkButton(self, text="Search Users", command=lambda: controller.show_frame(searchUsers))
-        user_button.place(relx=0.3, rely=0.1, anchor=CENTER)
-
-        friend_button = CTkButton(self, text="Make a Friend", command=lambda: controller.show_frame(makeFriend))
-        friend_button.place(relx=0.5, rely=0.1, anchor=CENTER)
-
-        review_button = CTkButton(self, text="Review a Business", command=lambda: controller.show_frame(reviewBusiness))
-        review_button.place(relx=0.7, rely=0.1, anchor=CENTER)
+        load_buttons(self, controller)
 
 
 # cursor.execute('SELECT TOP(10) * FROM dbo.User_yelp')
@@ -169,10 +148,6 @@ class reviewBusiness(CTkFrame):
 # ## Iterate through the returned rows
 # for row in cursor:
 #     print('row:', row)
-
-# conn = pyodbc.connect('driver={ODBC Driver 18 for SQL Server};server=cypress.csil.sfu.ca;uid=s_sdh13;pwd=7eFr4EfA3bATTLn7;Encrypt=yes;TrustServerCertificate=yes')
-
-# cursor = conn.cursor()
 
 gui = yelp_gui()
 gui.mainloop()
